d1bc0533799ec0d30c6a43d25da3d09fe87a14f5
Vedran Tepavcevic <mr_vedran@hotmail.com>
2019-02-05 23:26:55 -0500

    Rework Javascript code with Async/await

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const fetchData = (action) => {
   return fetch(url)
 }
 
-// convert response to json
-function json(Response) {
+// convert response to json and return result property value
+const  json = async (Response) => {
   if (Response.ok) {
-    return Response.json()
+    let jsonResult = await Response.json()
+    return jsonResult.result
   }
   throw new Error('Api did not respond')
 }
@@ -18,8 +19,27 @@ const getBlockByNumber = (number) => {
   return `module=proxy&action=eth_getBlockByNumber&tag=${number}&boolean=true`
 }
 
-fetchData(getBlockByNumber("latest")).then(json).then(Response => {
-  // add to html
-  let lastBlock = document.getElementById("lastblockno")
-  lastBlock.textContent = Number(Response.result.number)
-})
+const  request = async () => {
+// set veriables for calculations
+const lastBlockInfo = await fetchData(getBlockByNumber("latest"))
+const Response1 = await json(lastBlockInfo)
+const nBlock = "0x"+ parseInt(Response1.number - 5000).toString(16)
+const nBlockInfo = await fetchData(getBlockByNumber(nBlock))
+const Response2 = await json(nBlockInfo)
+const avgBlockTime = (Response1.timestamp - Response2.timestamp) / 5000
+const avgDifficulty = (Number(Response1.difficulty) + Number(Response2.difficulty)) / 2
+const avghashRate = Response1.difficulty / avgBlockTime
+
+
+// add to html
+let lastBlock = document.getElementById("lastblockno")
+let Transactions = document.getElementById("transactionsno")
+let hashRate = document.getElementById("avghashrate")
+let difficulty = document.getElementById("avgdifficulty")
+lastBlock.textContent = Number(Response1.number)
+Transactions.textContent = Number(Response1.transactions.length)
+hashRate.textContent = `${(avghashRate / 1e9).toFixed(2)} GH/s` 
+difficulty.textContent = `${(avgDifficulty / 1e12).toFixed(2)} TH`
+}
+
+request()
